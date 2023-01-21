fec0ef544f28fec65d6147fa422d1e62f6cec6a8
alovew <anne@airbyte.io>
2023-01-20 18:21:11 -0800

    workflow client injection

--- airbyte-server/src/main/java/io/airbyte/server/ServerApp.java
+++ airbyte-server/src/main/java/io/airbyte/server/ServerApp.java
@@ -47,6 +47,7 @@ import io.airbyte.persistence.job.factory.OAuthConfigSupplier;
 import io.airbyte.persistence.job.tracker.JobTracker;
 import io.airbyte.validation.json.JsonSchemaValidator;
 import io.airbyte.workers.helper.ConnectionHelper;
+import io.temporal.client.WorkflowClient;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import java.net.http.HttpClient;
 import java.util.Map;
@@ -212,9 +213,10 @@ public class ServerApp implements ServerRunnable {
     final ConnectionManagerUtils connectionManagerUtils = new ConnectionManagerUtils();
     final StreamResetRecordsHelper streamResetRecordsHelper = new StreamResetRecordsHelper(jobPersistence, streamResetPersistence);
 
+    final WorkflowClient workflowClient = TemporalWorkflowUtils.createWorkflowClient(temporalService, temporalUtils.getNamespace());
     final TemporalClient temporalClient = new TemporalClient(
         configs.getWorkspaceRoot(),
-        TemporalWorkflowUtils.createWorkflowClient(temporalService, temporalUtils.getNamespace()),
+        workflowClient,
         temporalService,
         streamResetPersistence,
         connectionManagerUtils,
@@ -261,7 +263,7 @@ public class ServerApp implements ServerRunnable {
         configs.getLogConfigs(),
         eventRunner,
         connectionsHandler,
-        envVariableFeatureFlags);
+        envVariableFeatureFlags, workflowClient);
 
     final AirbyteProtocolVersionRange airbyteProtocolVersionRange = new AirbyteProtocolVersionRange(configs.getAirbyteProtocolVersionMin(),
         configs.getAirbyteProtocolVersionMax());
