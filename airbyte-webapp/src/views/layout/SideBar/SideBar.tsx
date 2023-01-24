6a932678867725ba4350b4e1a3f6420f8c7418c9
tealjulia <teal@airbyte.io>
2023-01-24 11:07:24 -0500

    test cleanup to not use classnames

--- airbyte-webapp/src/views/layout/SideBar/SideBar.tsx
+++ airbyte-webapp/src/views/layout/SideBar/SideBar.tsx
@@ -112,7 +112,7 @@ export const SideBar: React.FC<SideBarProps> = ({ additionalTopItems, bottomMenu
         {additionalTopItems}
         <ul className={styles.menu} data-testid="navMainItems">
           <li>
-            <NavLink className={navLinkClassName} to={RoutePaths.Connections}>
+            <NavLink className={navLinkClassName} to={RoutePaths.Connections} data-testid="connectionsLink">
               <ConnectionsIcon />
               <Text className={styles.text} size="sm">
                 <FormattedMessage id="sidebar.connections" />
@@ -120,7 +120,7 @@ export const SideBar: React.FC<SideBarProps> = ({ additionalTopItems, bottomMenu
             </NavLink>
           </li>
           <li>
-            <NavLink className={navLinkClassName} to={RoutePaths.Source}>
+            <NavLink className={navLinkClassName} to={RoutePaths.Source} data-testid="sourcesLink">
               <SourceIcon />
               <Text className={styles.text} size="sm">
                 <FormattedMessage id="sidebar.sources" />
@@ -128,7 +128,7 @@ export const SideBar: React.FC<SideBarProps> = ({ additionalTopItems, bottomMenu
             </NavLink>
           </li>
           <li>
-            <NavLink className={navLinkClassName} to={RoutePaths.Destination}>
+            <NavLink className={navLinkClassName} to={RoutePaths.Destination} data-testid="destinationsLink">
               <DestinationIcon />
               <Text className={styles.text} size="sm">
                 <FormattedMessage id="sidebar.destinations" />
